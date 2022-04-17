245571382c3cb2efb0b0cc008a1b3ae758c89f7c
henry <henrysoares.dev@gmail.com>
2022-04-17 16:24:09 -0300

    First commit.

---
+++ src/test/java/com/hencarvalho/freightsystem/FreightSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hencarvalho.freightsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreightSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
